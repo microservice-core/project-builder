0a69aed4a54ac808dfe52e32cf14339f5d3fa2d6
herve <herveberaud.pro@gmail.com>
2016-08-20 19:37:52 +0200

    adding my own template system

---
+++ packaging/sources/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from main import main
+
+__author__ = '{{ cookiecutter.full_name }}'
+__email__ = '{{ cookiecutter.email }}'
+__version__ = '{{ cookiecutter.version }}'
+
+def run():
+    main()
